0a2d06f0aeae94fb82bef4441b192111b4f4bf4e
joaocarloslima <joaocarloslima@me.com>
2025-02-28 09:43:31 -0300

    category: create, get

--- src/main/java/br/com/fiap/money_control_api/controller/CategoryController.java
+++ src/main/java/br/com/fiap/money_control_api/controller/CategoryController.java
@@ -1,7 +1,13 @@
 package br.com.fiap.money_control_api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.money_control_api.model.Category;
@@ -9,9 +15,33 @@ import br.com.fiap.money_control_api.model.Category;
 @RestController
 public class CategoryController {
 
-    @RequestMapping(path = "/category", produces = "application/json", method = {RequestMethod.GET})
-	public Category index(){
-		return new Category(1L, "Educação", "book");
+	private List<Category> repository = new ArrayList<>();
+
+	@GetMapping("/categories")
+	public List<Category> index() {
+		return repository;
 	}
-    
+
+	@PostMapping("/categories")
+	// @ResponseStatus(code = HttpStatus.CREATED)
+	public ResponseEntity<Category> create(@RequestBody Category category) {
+		System.out.println("Cadastrando categoria " + category.getName());
+		repository.add(category);
+		return ResponseEntity.status(201).body(category);
+	}
+
+	@GetMapping("/categories/{id}")
+	public ResponseEntity<Category> get(@PathVariable Long id) {
+		System.out.println("buscando categoria " + id);
+		var category = repository.stream()
+				.filter(c -> c.getId().equals(id))
+				.findFirst();
+
+		if (category.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+
+		return ResponseEntity.ok(category.get());
+	}
+
 }
