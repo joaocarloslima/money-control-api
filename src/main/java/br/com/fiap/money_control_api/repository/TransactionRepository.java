f3a3fa62851e05b121faeeac877f751daf5079f8
joaocarloslima <joaocarloslima@me.com>
2025-04-11 09:08:54 -0300

    add filter by date, pagination and sort

--- src/main/java/br/com/fiap/money_control_api/repository/TransactionRepository.java
+++ src/main/java/br/com/fiap/money_control_api/repository/TransactionRepository.java
@@ -1,18 +1,19 @@
 package br.com.fiap.money_control_api.repository;
 
-import java.time.LocalDate;
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import br.com.fiap.money_control_api.model.Transaction;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Long>{
+public interface TransactionRepository extends
+        JpaRepository<Transaction, Long>, JpaSpecificationExecutor<Transaction> {
 
-    //List<Transaction> findByDescriptionContainingIgnoringCase(String description);
+    // List<Transaction> findByDescriptionContainingIgnoringCase(String
+    // description);
 
-    //List<Transaction> findByDescriptionContainingIgnoringCaseAndDate(String description, LocalDate date);
+    // List<Transaction> findByDescriptionContainingIgnoringCaseAndDate(String
+    // description, LocalDate date);
 
-   //List<Transaction> findByDate(LocalDate date);
-    
+    // List<Transaction> findByDate(LocalDate date);
+
 }
